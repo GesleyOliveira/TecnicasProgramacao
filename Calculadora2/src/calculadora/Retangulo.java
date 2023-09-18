addc31d789edd3008165cc8294301d37591fb901
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2023-09-17 22:42:10 -0300

    classes para calcular triangulo e retangulo

--- Calculadora2/src/calculadora/Retangulo.java
+++ Calculadora2/src/calculadora/Retangulo.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadora;
 
-/**
- *
- * @author User
- */
+enum FormasGeometricas{
+    QUADRADO, 
+    RETANGULO
+}
+
 public class Retangulo {
+    private double area, perimetro, lado1, lado2;
+    public FormasGeometricas tipo;
+    
+    public void setLado(double lado){
+        this.lado1 = lado;
+    }
+    
+    public double getLado1(){
+        return lado1;
+    }
+    
+    public double getLado2(){
+        return lado2;
+    }
+    
+    public double getArea(){
+        return area;
+    }
+    
+    public double getPerimetro(){
+        return perimetro;
+    }
+    
+     public FormasGeometricas getTipo() {
+        return tipo;
+    }
+    
+    public Retangulo(double lado){
+        this.lado1 = lado;
+        this.lado2 = lado; 
+        this.tipo = FormasGeometricas.QUADRADO;       
+    }
+    
+    public Retangulo(double lado1, double lado2){
+        this(lado1);
+        this.lado2 = lado2;
+        this.area = lado1 * lado2;
+        this.tipo = FormasGeometricas.RETANGULO;
+    }
+    
+    public String getQuadrado(){
+        return ("Quadrado com .... de lado, perímetro de .... e área de ....");
+    }
+    
+    public String getRetangulo(){
+        return ("Retangulo com .... por .... de lado, perímetro de .... e área de ....");
+    }
     
 }
--- Calculadora2/src/calculadora/Triangulo.java
+++ Calculadora2/src/calculadora/Triangulo.java
@@ -1,13 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadora;
 
-/**
- *
- * @author User
- */
+import java.lang.Math;
+
+enum TipoTriangulo{
+    TRIANGULO_RETANGULO,
+    TRIANGULO_OBTUSANGULO,
+    TRIANGULO_ACUTANGULO
+}
+
 public class Triangulo {
+    private double base, altura, angulo, segmentoAB, segmentoAC;
+    private TipoTriangulo tipo ;
     
+    
+    public Triangulo(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+        this.angulo = 90;
+        classificarTriangulo();
+    }
+    
+    public Triangulo(double segmentoAB, double segmentoAC, double angulo){
+        this.segmentoAB = segmentoAB;
+        this.segmentoAC = segmentoAC;
+        
+        if (angulo <= 0 || angulo > 90){
+            this.angulo = 90;
+        }
+        else{
+            this.angulo = angulo;
+        }
+        classificarTriangulo();
+    }
+    
+    private void classificarTriangulo(){
+        if (angulo == 90){
+            tipo = TipoTriangulo.TRIANGULO_RETANGULO;
+        } else if (angulo > 90) {
+            tipo = TipoTriangulo.TRIANGULO_OBTUSANGULO;
+        } else {
+            tipo = TipoTriangulo.TRIANGULO_ACUTANGULO;
+        }
+    } 
+    
+    private double calcularPerimetro() {
+        double cosAngulo = Math.cos(Math.toRadians(angulo));
+        return segmentoAB + segmentoAC + 2 * Math.sqrt(segmentoAB * segmentoAC * (1-cosAngulo));
+    }
+    
+    private double calcularArea() {
+        double senAngulo = Math.sin(Math.toRadians(angulo));
+        return (0.5) * segmentoAB * segmentoAC * senAngulo;
+    }
+    
+    public String getDescricao() {
+        String tipoDescricao = "";
+        switch (tipo) {
+            case TRIANGULO_RETANGULO:
+                tipoDescricao = "Triângulo Retângulo";
+                break;
+            case TRIANGULO_OBTUSANGULO:
+                tipoDescricao = "Triângulo Obtusângulo";
+                break;
+            case TRIANGULO_ACUTANGULO:
+                tipoDescricao = "Triângulo Acutângulo";
+                break;
+        }
+        return tipoDescricao + " com base de " + base + ", altura de " + altura +
+            ", perímetro de " + calcularPerimetro() + " e área de " + calcularArea();
+    }
+       
 }
